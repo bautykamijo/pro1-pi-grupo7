63f0a46eca612ddd279f34fd0f1e4acfb513c721
ArielAzambuja <aazambuja@udesa.edu.ar>
2022-11-18 16:35:11 -0300

    favoritos series

--- js/favorites.js
+++ js/favorites.js
@@ -13,7 +13,7 @@ console.log(peliculasFav);
 /* EVALUAR SI EL ARRAY TIENE 0 ELEMENTOS o si viene nulo */
 
 if (peliculasFav == null || peliculasFav.length == 0) {
-    section.innerHTML = '<p>No hay peliculas en favoritos</p>'
+    section.innerHTML = '<p>No tienes peliculas favoritas</p>'
 }
 else {
     for (let i = 0; i < peliculasFav.length; i++) {
@@ -33,6 +33,52 @@ else {
                                 </article> `
 
             section.innerHTML = favoritos;
+
+            return data;
+        })
+        .catch(function (error) {
+            console.log(error);
+            return error;
+        });
+
+        
+        
+    }}
+
+
+let recuperoStorageser = localStorage.getItem('seriesFav');
+
+
+let seriesFav = JSON.parse(recuperoStorageser);
+
+
+let sectionn = document.querySelector(".seriesFav");
+let favoritoss = ''; 
+console.log(seriesFav);
+
+/* EVALUAR SI EL ARRAY TIENE 0 ELEMENTOS o si viene nulo */
+
+if (seriesFav == null || seriesFav.length == 0) {
+    sectionn.innerHTML = '<p>No tiene series favoritas</p>'
+}
+else {
+    for (let i = 0; i < seriesFav.length; i++) {
+        let url =  `https://api.themoviedb.org/3/tv/${seriesFav[i]}?api_key=${apiKey}&language=en-US`;
+
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data){
+            
+            favoritoss += ` <article class="cuadrado thor"> 
+                                <a class="aclickeo" href="./detail-serie.html?idSeries=${data.id}">
+                                <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="" class="img1">
+                                ${data.name} (${data.first_air_date})
+                                </a>
+                                </article> `
+
+            sectionn.innerHTML = favoritoss;
 
             return data;
         })
