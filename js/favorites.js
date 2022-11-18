b1403213eb82b508def90d52f6c9737d23216fbb
bautykamijo <bautykamijo@gmail.com>
2022-11-18 17:14:02 -0300

    pulpa

--- js/favorites.js
+++ js/favorites.js
@@ -7,7 +7,7 @@ let peliculasFav = JSON.parse(recuperoStorage);
 
 
 let section = document.querySelector(".pelisFav");
-let favoritos = ''; 
+let favoritos = '';
 console.log(peliculasFav);
 
 
@@ -16,30 +16,33 @@ if (peliculasFav == null || peliculasFav.length == 0) {
 }
 else {
     for (let i = 0; i < peliculasFav.length; i++) {
-        let url =  `https://api.themoviedb.org/3/movie/${peliculasFav[i]}?api_key=${apiKey}&language=en-US`;
+        if (i < 5) {
+            let url = `https://api.themoviedb.org/3/movie/${peliculasFav[i]}?api_key=${apiKey}&language=en-US`;
 
-    fetch(url)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data){
-            
-            favoritos += ` <article class="cuadrado thor"> 
+            fetch(url)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+
+                    favoritos += ` <article class="cuadrado thor"> 
                                 <a class="aclickeo" href="./detail-movie.html?idPelicula=${data.id}">
                                 <img src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="" class="img1">
                                 ${data.title} (${data.release_date})
                                 </a>
                                 </article> `
 
-            section.innerHTML = favoritos;
+                    section.innerHTML = favoritos;
 
-            return data;
-        })
-        .catch(function (error) {
-            console.log(error);
-            return error;
-        });
+                    return data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                    return error;
+                });
+        }
 
-        
-        
-    }}
+
+
+    }
+}
